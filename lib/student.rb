3f5ae6e7a0d0d1728b07b2449ed345af454e3f2d
Caroline Longnecker <cvrieous@gmail.com>
2020-06-07 17:43:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,7 +20,7 @@ class Student
   
   def self.drop_table()
     sql = <<-SQL
-      DROP TABLE IF NOT EXISTS students
+      DROP TABLE IF NOT EXISTS students;
       SQL
     DB[:conn].execute(sql)
   end
