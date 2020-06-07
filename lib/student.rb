f8b7e391625b88f9b2414517d1f1705a6dc388fb
Caroline Longnecker <cvrieous@gmail.com>
2020-06-07 17:36:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,6 +2,7 @@ class Student
   attr_reader id
   attr_accessor name, grade
 
+  def initialize
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
