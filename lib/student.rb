ccc5ad7f2894fc8c02681037fdec57ced459ed0e
Caroline Longnecker <cvrieous@gmail.com>
2020-06-07 17:51:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -16,7 +16,7 @@ class Student
     sql = <<-SQL
       SELECT id FROM students ORDER BY id DESC LIMIT 1
     SQL
-    @id = DB[:conn].execute(sql)
+    @id = DB[:conn].execute(sql).
   end
   
   def self.create_table()
